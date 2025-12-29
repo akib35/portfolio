af716c2771ffde7b856f9a83beabfa7dde08a0c6
akib-cdda <akib@cdda.io>
2025-12-29 17:17:47 +0600

    add .gitignore, rename HTML and JS files, update contact info, and add project descriptions

--- temp/script.js
+++ temp/script.js
@@ -31,3 +31,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('year').textContent = new Date().getFullYear();
+});
